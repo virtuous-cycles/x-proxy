0518d6dc27e307887c07dc9178f011511a944e38
Constellate Accounts <35009394-constellate@users.noreply.replit.com>
2024-08-07 22:30:44 +0000

    Remove post_reply from __init__.py

--- api/__init__.py
+++ api/__init__.py
@@ -2,7 +2,6 @@ from flask import Blueprint
 
 api_bp = Blueprint('api', __name__)
 
-from . import post_reply_route
 from . import pull_mentions_route
 from . import post_tweet_route
 from . import get_tweet_route
