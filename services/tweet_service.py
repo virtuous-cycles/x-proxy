68cb9148543b0a1c6a15a6afc8439450ed2714b0
Constellate Accounts <35009394-constellate@users.noreply.replit.com>
2024-08-08 00:12:32 +0000

    Set assign TT id for pull_mentions in tweet_service

--- services/tweet_service.py
+++ services/tweet_service.py
@@ -18,7 +18,9 @@ class TweetService:
         client = self.oauth2_handler.get_client()
         response = client.get_users_mentions(
             id=Config.TRUTH_TERMINAL_TWITTER_ID,
-            max_results=15,
+            max_results=15, # default is 10
+            # since_id (int | str | None) – Returns results with a Tweet ID greater than (that is, more recent than) the specified ‘since’ Tweet ID. There are limits to the number of Tweets that can be accessed through the API. If the limit of Tweets has occurred since the since_id, the since_id will be forced to the oldest ID available.
+            # start_time (datetime.datetime | str | None) – YYYY-MM-DDTHH:mm:ssZ (ISO 8601/RFC 3339). The oldest UTC timestamp from which the Tweets will be provided. Timestamp is in second granularity and is inclusive (for example, 12:00:01 includes the first second of the minute).
             expansions=[
                 'author_id', 
                 'referenced_tweets.id',
