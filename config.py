21deb06e4b4e830841029cbe99a89986373956ed
Constellate Accounts <35009394-constellate@users.noreply.replit.com>
2024-08-08 02:47:20 +0000

    Switch to TT credentials

--- config.py
+++ config.py
@@ -9,10 +9,10 @@ class Config:
     # OAuth 1.0a credentials
     CONSUMER_KEY = os.environ['consumer_key']
     CONSUMER_SECRET = os.environ['consumer_secret']
-    # ACCESS_TOKEN = os.environ['access_token']
-    ACCESS_TOKEN = os.environ['const_labs_access_token']
-    # ACCESS_TOKEN_SECRET = os.environ['access_token_secret']
-    ACCESS_TOKEN_SECRET = os.environ['const_labs_access_token_secret']
+    ACCESS_TOKEN = os.environ['access_token']
+    ACCESS_TOKEN_SECRET = os.environ['access_token_secret']
+    # ACCESS_TOKEN = os.environ['const_labs_access_token']
+    # ACCESS_TOKEN_SECRET = os.environ['const_labs_access_token_secret']
 
     # Other configurations
     API_SECRET_KEY = os.environ['API_SECRET_KEY']
